25cf3b9796e68a1496781692a808d3fc54861a22
Breno M. Silva <breno@bussoladoinvestidor.com>
2021-09-27 11:27:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var y = 11;
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  y -= 1;
+
+  // Display the result in the element with id="demo"
+  document.getElementById("timer").innerHTML = y;
+
+  // If the count down is finished, write some text
+  if (y < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "\n\nHeitor";
+    document.getElementById("clock").style = "border: none; background-color: white;";
+    document.getElementById("container").style = "background: lightblue; background-image: url('./img/fireworks.gif');";
+  }
+}, 1000);
